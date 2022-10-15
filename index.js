cb1c5f36b8db60d32769ff3d488bb06ec3c44e8a
Oksana <example@gmail.com>
2022-10-16 00:22:09 +0300

    addFieldSet method

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 class Checkbox {
-  constructor(id, classCheckbox, name, dataOn, dataOff) {
+  constructor(id, classCheckbox, name, dataOn, dataOff, checked='') {
     this.id = id;
     this.classCheckbox = classCheckbox;
     this.name = name;
     this.dataOn = dataOn;
     this.dataOff = dataOff;
+    this.checked=checked;
   }
   createCheckbox(id) {}
   render() {
@@ -12,7 +13,8 @@ class Checkbox {
                 type="radio" 
                 id=${this.id} 
                 name=${this.name} 
-                class=${this.classCheckbox}> 
+                class=${this.classCheckbox}
+                checked=${this.checked}> 
             <label 
                 for=${this.id} 
                 data-on-label=${this.dataOn} 
@@ -29,7 +31,13 @@ class Form {
     this.form = document.querySelector(this.classForm);
   }
 
-  addFieldSet(fieldSetClass) {
+  addFieldSet(fieldSetClass,legendText='') {
+    const field=this.form.appendChild(document.createElement("fieldset"));
+    field.className = fieldSetClass;
+    field.appendChild(document.createElement('legend')).innerText=`${legendText}`;
+  }
+
+  addLegend(fieldSetClass) {
     this.form.appendChild(document.createElement("fieldset"))
     .className = fieldSetClass;
   }
@@ -44,15 +52,27 @@ class Form {
   }
 }
 
-let checkbox = new Checkbox("demo1", "age", "check", "on", "щаа");
-let checkbox2 = new Checkbox("demo2", "sex", "check2", "male", "female");
-let checkbox3 = new Checkbox("demo3", "sex", "check2", "up", "down");
+let female = new Checkbox("demo1", "sex", "check", "female", "female");
+let male = new Checkbox("demo2", "sex", "check", "male", "male");
+let all = new Checkbox("demo3", "sex", "check", "all", "all");
+
+let ageUp = new Checkbox("demo4", "sex", "check1", "age-up", "age-up");
+let ageDown = new Checkbox("demo5", "sex", "check1", "age-down", "age-down");
+let nameUp = new Checkbox("demo6", "sex", "check1", "name-up", "name-up");
+let nameDown = new Checkbox("demo7", "sex", "check1", "name-down", "name-down");
+let unsorted = new Checkbox("demo8", "sex", "check1", "unsorted", "unsorted");
 
 const myForm = new Form(".container", "form");
 
-myForm.addFieldSet("field1");
-myForm.addFieldSet("field2");
+myForm.addFieldSet("field1",'Filter by gender');
+myForm.addFieldSet("field2","Sorting");
 
-myForm.addCheckbox(checkbox, '.field1');
-myForm.addCheckbox(checkbox2, '.field2');
-myForm.addCheckbox(checkbox3, '.field2');
+myForm.addCheckbox(female, '.field1');
+myForm.addCheckbox(male, '.field1');
+myForm.addCheckbox(all, '.field1');
+
+myForm.addCheckbox(ageUp, '.field2');
+myForm.addCheckbox(ageDown, '.field2');
+myForm.addCheckbox(nameUp, '.field2');
+myForm.addCheckbox(nameDown, '.field2');
+myForm.addCheckbox(unsorted, '.field2');
